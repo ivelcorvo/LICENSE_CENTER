d517e1033067bbc0e52035557c0d6610133af2dd
ivelcorvo <levialvesjr@hotmail.com>
2026-01-27 08:36:31 -0300

    feat: estilo inicial do projeto

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -3,7 +3,13 @@ import { Outlet } from "react-router";
 export default function RootLayout(){
   return(
 
-    <Outlet />
+    <div className="bg-gray-950 text-gray-300 min-h-screen flex">
+      <main className="flex flex-col flex-1 justify-center items-center px-4 py-8">
+        <section>
+          <Outlet />
+        </section>
+      </main>
+    </div>      
 
   );
 };
